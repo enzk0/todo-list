fa65b32b708f807750c6ce2ca8331ec2bb210e47
enzk0 <renzoroel24@gmail.com>
2025-06-12 20:55:38 +0800

    IMPL: Setup functions for export

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -2,6 +2,21 @@ export const getNotes = async (req, res) => {
     return res.send("Get all notes");
 };
 
+export const getNote = async (req, res) => {
+    const { id } = req.params;
+    return res.send(`Get note with ID: ${id}`);
+};
+
 export const createNote = async (req, res) => {
     return res.send("Create a note");
+};
+
+export const updateNote = async (req, res) => {
+    const { id } = req.params;
+    return res.send(`Update note with ID: ${id}`);
+};
+
+export const deleteNote = async (req, res) => {
+    const { id } = req.params;
+    return res.send(`Delete note with ID: ${id}`);
 };
